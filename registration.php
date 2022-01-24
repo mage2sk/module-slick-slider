eabe9cc7f1070caf590ef89d1bdb2abe7e3c97a4
Kishan Savaliya <kishansavaliyakb@gmail.com>
2022-01-24 13:25:42 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Mage2SK Use Slick slider in Magento 2
+ * Mage2sk Use Slick slider in Magento 2
  *
- * @package      Mage2SK_SlickSlider
+ * @package      Mage2sk_SlickSlider
  * @author       Kishan Savaliya <kishansavaliyakb@gmail.com>
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Mage2SK_SlickSlider',
+    'Mage2sk_SlickSlider',
     __DIR__
 );
